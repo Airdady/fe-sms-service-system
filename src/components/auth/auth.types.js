7f721f520a5c196e877a7927624d5313f52110de
swaibu <sswaib11@gmail.com>
2021-05-27 00:45:13 +0300

    remove react app

--- src/components/auth/auth.types.js
+++
@@ -1,7 +0,0 @@
-const types = {
-  GET_USER_DETAILS: 'GET_USER_DETAILS',
-  LOGOUT: 'LOGOUT',
-  LOGIN: 'LOGIN',
-};
-
-export default types;
--- src/redux/types.js
+++
@@ -1,6 +0,0 @@
-const types = {
-  GET_USER_DETAILS: 'GET_USER_DETAILS',
-  LOGOUT: 'LOGOUT',
-};
-
-export default types;
