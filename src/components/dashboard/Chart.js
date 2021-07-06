0fe64af0805397b9dc5364b2bc596be601730245
isiagi <isiagigeofrey0@gmail.com>
2021-07-06 12:11:59 +0300

    adds chart data

--- src/components/dashboard/Chart.js
+++ src/components/dashboard/Chart.js
@@ -36,7 +36,7 @@ export default function Chart() {
       <Title>Today</Title>
       <ResponsiveContainer>
         <LineChart width={500} height={300} data={work}>
-          <XAxis dataKey="date" />
+          <XAxis dataKey="date"/>
           <YAxis />
           <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
           <Line type="monotone" dataKey="sms" stroke="#8884d8" />
