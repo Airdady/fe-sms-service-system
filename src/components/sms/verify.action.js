60fd841e06ac87994f185379f23aec577c3346d1
isiagi <isiagigeofrey0@gmail.com>
2021-07-09 06:05:11 +0300

    fixes sms fetch

--- src/components/sms/verify.action.js
+++ src/components/sms/verify.action.js
@@ -4,7 +4,7 @@ import Service from '../../service';
 const VerifyProfile = {
   async CreateOtpProfile(data) {
     try {
-      const res = await Service.createOtpProfile(data);
+      const res = await Service.createSmsProfile(data);
       console.log(res.data);
       return {
         type: Types.CREATE_VERIFY_SUCCESS,
@@ -33,7 +33,7 @@ const VerifyProfile = {
   },
   async deleteOtpProfile(id) {
     try {
-      await Service.deleteOtpProfile(id);
+      await Service.deleteSmsProfile(id);
       return {
         type: Types.DEL_VERIFY_PROFILE_SUCCESS,
         id,
@@ -47,7 +47,7 @@ const VerifyProfile = {
   },
   async updateOtpProfile(id, data) {
     try {
-      await Service.updateOtpProfile(id, data);
+      await Service.updateSmsProfile(id, data);
       return {
         type: Types.UPDATE_VERIFY_PROFILE_SUCCESS,
         data: { id, active: data.active }
