28a2e4a6d968074e833bfef4738ed894f87ce1f8
isiagi <isiagigeofrey0@gmail.com>
2021-07-09 06:00:50 +0300

    adds verify profile route

--- src/service/index.js
+++ src/service/index.js
@@ -8,11 +8,11 @@ const Api = {
   getLogs: () => Router.get('/api_logs/'),
   getStats: () => Router.get('/stats/'),
   deleteOtpProfile: (id) => Router.delete(`/verify_profile/${id}`),
-  updateOtpProfile: (id, data) => Router.patch(`/verify/${id}`, data),
-  createSmsProfile: (data) => Router.post('/sms', data),
-  getSmsProfile: () => Router.get('/sms'),
-  deleteSmsProfile: (id) => Router.delete(`/sms/${id}`),
-  updateSmsProfile: (id, data) => Router.patch(`/sms/${id}`, data),
+  updateOtpProfile: (id, data) => Router.patch(`/verify_profile/${id}`, data),
+  createSmsProfile: (data) => Router.post('/sms_profile', data),
+  getSmsProfile: () => Router.get('/sms_profile'),
+  deleteSmsProfile: (id) => Router.delete(`/sms_profile/${id}`),
+  updateSmsProfile: (id, data) => Router.patch(`/sms_profile/${id}`, data),
 };
 
 export default Api;
