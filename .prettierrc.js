27c841ae3a63f2d8391ce43b992ab7074e30063e
simonojok19 <simonojok19@gmail.com>
2021-08-03 11:47:50 +0300

    added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "linebreak-style": ["error", "unix"],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
--- src/components/contactus/index.js
+++ src/components/contactus/index.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, Box} from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Footer from '../home/Footer';
-import Form from './form';
-import Map from './map'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography, Box } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Footer from "../home/Footer";
+import Form from "./form";
+import Map from "./map";
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    backgroundSize: 'cover',
+    width: "100%",
+    backgroundSize: "cover",
     background:
-      'url(https://numverify.com/images/promo_bgs/numverify_promo_bg.jpg)',
-    '& .hljs': {
+      "url(https://numverify.com/images/promo_bgs/numverify_promo_bg.jpg)",
+    "& .hljs": {
       borderRadius: theme.shape.borderRadius,
     },
   },
   table: {
     minWidth: 350,
-    marginTop: '1rem',
+    marginTop: "1rem",
   },
   cardContent: {
-    marginTop: '-8rem',
-    position: 'relative',
+    marginTop: "-8rem",
+    position: "relative",
     zIndex: 100,
-    padding: '1rem',
+    padding: "1rem",
   },
   bgColor: {
-    width: '100%',
-    height: '250px',
-    background: 'transparent',
+    width: "100%",
+    height: "250px",
+    background: "transparent",
   },
   rightBtn: {
-    marginLeft: '1rem',
+    marginLeft: "1rem",
     background: theme.palette.primary.dark,
   },
   popularPaper: {
-    padding: '1rem',
-    textAlign: 'center',
+    padding: "1rem",
+    textAlign: "center",
     borderLeft: 0,
-    background: 'transparent',
-    color: 'white',
+    background: "transparent",
+    color: "white",
   },
   centerContent: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'start',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "start",
   },
   goodies: {
-    width: '100%',
-    marginBottom: '3rem',
-    padding: '3rem 0',
+    width: "100%",
+    marginBottom: "3rem",
+    padding: "3rem 0",
   },
 }));
 
@@ -61,17 +61,17 @@ export default function BasicTable() {
       <div className={classes.root}>
         <Box display="flex" alignItems="center" className={classes.bgColor}>
           <Container maxWidth="lg">
-            <Grid container style={{ height: '100%' }}>
+            <Grid container style={{ height: "100%" }}>
               <Grid item md={12}>
                 <Box flexGrow={1} textAlign="center">
                   <Typography
                     variant="h3"
-                    style={{ color: 'white' }}
+                    style={{ color: "white" }}
                     gutterBottom
                   >
                     Contact Us
                   </Typography>
-                  <Typography variant="body1" style={{ color: 'white' }}>
+                  <Typography variant="body1" style={{ color: "white" }}>
                     Drop any query OR Request for quotation respond as soon as
                     possible
                   </Typography>
@@ -84,9 +84,9 @@ export default function BasicTable() {
       <Box p={5} width="100%" bgcolor="#f0f3fb" textAlign="left">
         <Container>
           <Grid container spacing={3}>
-            <Grid item md={6} >
+            <Grid item md={6}>
               <Box mb={3}>
-                <Map/>
+                <Map />
               </Box>
               <Grid item md={6}>
                 <Typography>
